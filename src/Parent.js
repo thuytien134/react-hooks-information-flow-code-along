6e46c1035d44c58d5844f50a9f5b72fcb14888e8
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2023-03-30 21:58:14 -0700

    updated

--- src/Parent.js
+++ src/Parent.js
@@ -6,10 +6,14 @@ function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
+  function handleChangeColor() {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); // update color state to a new value
+  }
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={handleChangeColor}/>
+      <Child onChangeColor={handleChangeColor}/>
     </div>
   );
 }
